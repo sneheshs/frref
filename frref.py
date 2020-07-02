2bbc7d8f22cf79bbe11365e3331cfbd7c0077224
Snehesh Shrestha <snehesh@umd.edu>
2020-07-02 12:02:54 -0400

    Added arguments and implemented tolerance calculations + some formatting

--- frref.py
+++ frref.py
@@ -1,7 +1,8 @@
 import numpy as np
+from scipy.sparse import isspmatrix  # ,csr_matrix
 
 
-def frref(A, tol=1e-6):
+def frref(A, TOL=None, TYPE=''):
     '''
     %FRREF   Fast reduced row echelon form.
     %   R = FRREF(A) produces the reduced row echelon form of A.
@@ -17,7 +18,7 @@ def frref(A, tol=1e-6):
     %   size of A. The speed-up is quite considerable if the number of columns in
     %   A is considerably larger than the number of its rows or when A is not dense.
     %
-    %   For sparse matrices, the algorithm ignores the tol value and uses sparse
+    %   For sparse matrices, the algorithm ignores the TOL value and uses sparse
     %   QR to compute the rref form, improving the speed by a few orders of
     %   magnitude.
     %
@@ -34,106 +35,109 @@ def frref(A, tol=1e-6):
     m = np.shape(A)[0]
     n = np.shape(A)[1]
 
-    # % Loop over the entire matrix.
-    i = 0
-    j = 0
-    jb = []
+    # Process Arguments
+    # ----------------------------------------------------------
+    # TOLERENCE
+    # % Compute the default tolerance if none was provided.
+    if TOL is None:
+        # Prior commit had TOL default to 1e-6
+        # TOL = max(m,n)*eps(class(A))*norm(A,'inf')
+        TOL = max(m, n)*np.spacing(type(A)(1))*np.linalg.norm(A, np.inf)
 
-    while (i < m) and (j < n):
-        # % Find value and index of largest element in the remainder of column j.
-        #p, k = np.max(np.abs(A[i:m, j]))
-        abscol = np.array(np.abs(A[i:m, j]))
-        p = np.max(abscol)
-        k = np.argmax(abscol, axis=0)
-        if np.ndim(k) > 1:
-            k = k[0]
+    # TYPE -- Sparce (s) or non-Sparce (Full, f)
+    if TYPE == '':   # set TYPE if sparse or not
+        if isspmatrix(A):
+            TYPE = 's'
         else:
-            k = int(k)
+            TYPE = 'f'
+    else:   # Set type
+        if not type(TYPE) is str or len(TYPE) > 1:  # Check valid type
+            print('Unknown matrix TYPE! Use "f" for full and "s" for sparse matrices.')
+            exit()
 
-        k = k+i #-1 #python zero index, not needed
+        TYPE = TYPE.lower()
+        if not TYPE == 'f' and not TYPE == 's':
+            print(
+                'Unknown matrix TYPE! Use ''f'' for full and ''s'' for sparse matrices.')
+            exit()
 
-        if p <= tol:
-            # % The column is negligible, zero it out.
-            A[i:m, j] = 0  # %(faster for sparse) %zeros(m-i+1,1);
-            j += 1
-        else:
-            # % Remember column index
-            jb.append(j)
+    # Non-Sparse
+    # ----------------------------------------------------------
+    if not isspmatrix(A) or TYPE == 'f':
+        # % Loop over the entire matrix.
+        i = 0
+        j = 0
+        jb = []
 
-            # % Swap i-th and k-th rows.
-            A = np.array(A)
-            A[[i, k], j:n] = A[[k, i], j:n]
+        while (i < m) and (j < n):
+            # % Find value (p) and index (k) of largest element in the remainder of column j.
+            abscol = np.array(np.abs(A[i:m, j]))
+            p = np.max(abscol)
+            k = np.argmax(abscol, axis=0)
+            if np.ndim(k) > 1:
+                k = k[0]
+            else:
+                k = int(k)
 
-            # % Divide the pivot row by the pivot element.
-            Ai = np.nan_to_num(A[i, j:n]/ A[i, j])
-            Ai = np.matrix(Ai).T.T
+            k = k+i  # -1 #python zero index, not needed
 
-            # % Subtract multiples of the pivot row from all the other rows.
-            A[:, j:n] = A[:, j:n] - np.dot(A[:, [j]], Ai)
-            A[i, j:n] = Ai
-            i += 1
-            j += 1
+            if p <= TOL:
+                # % The column is negligible, zero it out.
+                A[i:m, j] = 0  # %(faster for sparse) %zeros(m-i+1,1);
+                j += 1
+            else:
+                # % Remember column index
+                jb.append(j)
 
-    return A, jb
+                # % Swap i-th and k-th rows.
+                A = np.array(A)
+                A[[i, k], j:n] = A[[k, i], j:n]
 
+                # % Divide the pivot row by the pivot element.
+                Ai = np.nan_to_num(A[i, j:n] / A[i, j])
+                Ai = np.matrix(Ai).T.T
 
-# Only do_full implemented. TODO: The remaining of the function
-def frref_orig(A, tol, type):
-    m = np.shape(A)[0]
-    n = np.shape(A)[1]
+                # % Subtract multiples of the pivot row from all the other rows.
+                A[:, j:n] = A[:, j:n] - np.dot(A[:, [j]], Ai)
+                A[i, j:n] = Ai
+                i += 1
+                j += 1
 
-# switch nargin
-#   case 1,
-#     % Compute the default tolerance if none was provided.
-#     tol = max(m,n)*eps(class(A))*norm(A,'inf');
-#     if issparse(A)
-#       type = 's';
-#     else
-#       type = 'f';
-#     end
-#   case 2,
-#   if issparse(A)
-#     type = 's';
-#   else
-#     type = 'f';
-#   end
-#   case 3,
-#     if ~ischar(type)
-#       error('Unknown matrix TYPE! Use ''f'' for full and ''s'' for sparse matrices.')
-#     end
-#     type = lower(type);
-#     if ~strcmp(type,'f') && ~strcmp(type,'s')
-#       error('Unknown matrix TYPE! Use ''f'' for full and ''s'' for sparse matrices.')
-#     end
-# end
+        return A, jb
+
+    # Sparse
+    # ----------------------------------------------------------
+    else:
+        return None, None
 
 
-# do_full = ~issparse(A) || strcmp(type,'f');
+def frref_orig(A, TOL, TYPE):
+    # Only do_full implemented. TODO: The remaining of the function
+    return frref(A, TOL, TYPE)
+    '''
+    else
+        % Non-pivoted Q-less QR decomposition computed by Matlab actually
+        % produces the right structure (similar to rref) to identify independent
+        % columns.
+        R = qr(A);
 
-# if do_full
-    return frref(A, tol)
-# else
-#     % Non-pivoted Q-less QR decomposition computed by Matlab actually
-#     % produces the right structure (similar to rref) to identify independent
-#     % columns.
-#     R = qr(A);
+        % i_dep = pivot columns = dependent variables
+        %       = left-most non-zero column (if any) in each row
+        % indep_rows (binary vector) = non-zero rows of R
+        [indep_rows, i_dep] = max(R ~= 0, [], 2);
+        indep_rows = full(indep_rows); % probably more efficient
+        i_dep = i_dep(indep_rows);
+        i_indep = setdiff(1:n, i_dep);
 
-#     % i_dep = pivot columns = dependent variables
-#     %       = left-most non-zero column (if any) in each row
-#     % indep_rows (binary vector) = non-zero rows of R
-#     [indep_rows, i_dep] = max(R ~= 0, [], 2);
-#     indep_rows = full(indep_rows); % probably more efficient
-#     i_dep = i_dep(indep_rows);
-#     i_indep = setdiff(1:n, i_dep);
+        % solve R(indep_rows, i_dep) x = R(indep_rows, i_indep)
+        %   to eliminate all the i_dep columns
+        %   (i.e. we want F(indep_rows, i_dep) = Identity)
+        F = sparse([],[],[], m, n);
+        F(indep_rows, i_indep) = R(indep_rows, i_dep) \ R(indep_rows, i_indep);
+        F(indep_rows, i_dep) = speye(length(i_dep));
 
-#     % solve R(indep_rows, i_dep) x = R(indep_rows, i_indep)
-#     %   to eliminate all the i_dep columns
-#     %   (i.e. we want F(indep_rows, i_dep) = Identity)
-#     F = sparse([],[],[], m, n);
-#     F(indep_rows, i_indep) = R(indep_rows, i_dep) \ R(indep_rows, i_indep);
-#     F(indep_rows, i_dep) = speye(length(i_dep));
-
-#     % result
-#     A = F;
-#     jb = i_dep;
-# end
+        % result
+        A = F;
+        jb = i_dep;
+    end
+    '''
